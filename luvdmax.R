23fd6c0c6a4928b7ed0a7a1e4b8e687c718f3ffe
nmolanog <nmolanog@gmail.com>
2019-03-29 08:46:56 -0500

    list of tributes by html done, next: build table with this info

--- luvdmax.R
+++ luvdmax.R
@@ -7,7 +7,7 @@ rm(list=ls())
 options(stringsAsFactors = FALSE)
 options(max.print=999999)
 setwd("/home/nicolas/Desktop")
-
+library(tidyverse)
 library(xml2)
 library(rvest)
 library(stringr)
@@ -19,5 +19,10 @@ list_res<-list()
 for(i in seq_along(z0) ){
   webpage <- read_html(z0[i])
   list_res[[i]]<-data.frame(specs=html_nodes(webpage,"li.specs-item")%>%html_nodes("strong")%>%html_text() , 
-             value=html_nodes(webpage,"li.specs-item")%>%html_nodes("span")%>%html_text())
+                            value=html_nodes(webpage,"li.specs-item")%>%html_nodes("span")%>%html_text())
+  list_res[[i]]<-bind_rows(list_res[[i]],data.frame(specs=c("phone","precio"),
+                                                    value=c(html_nodes(webpage,"span.profile-info-phone-value")%>%str_extract_all("[0-9]+")%>%{map(.,~paste0(.,collapse=""))}%>%unlist%>%paste0(collapse=";"),
+                                                            html_nodes(webpage,"span.price-tag-fraction")%>%str_extract_all("[0-9]+")%>%unlist%>%paste(collapse=""))))
 }
+
+list_res%>%map(~.[,1])%>%reduce(union)->atributes_vec
