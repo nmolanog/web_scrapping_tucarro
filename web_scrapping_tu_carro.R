dadf2cb21a00b419545a26cc462096fb5e2f5b39
nmolanog <nmolanog@gmail.com>
2019-03-29 10:29:27 -0500

    testing table construction extracting url from search page results

--- web_scrapping_tu_carro.R
+++ web_scrapping_tu_carro.R
@@ -40,7 +40,7 @@ html_nodes(webpage,"ul.attribute-list")%>%html_nodes("li")%>%
 #######################
 rm(list=ls())
 options(max.print=999999)
-
+library(tidyverse)
 library(xml2)
 library(rvest)
 library(stringr)
@@ -56,4 +56,24 @@ html_nodes(webpage,"ol")%>% html_attr("id")
 
 ###pagination buttons
 html_nodes(webpage,"li.andes-pagination__button")%>%html_nodes("a")%>% html_attr("href")
+z0<-a
+list_res<-list()
+for(i in seq_along(z0)){
+  webpage <- read_html(z0[i])
+  list_res[[i]]<-data.frame(specs=html_nodes(webpage,"li.specs-item")%>%html_nodes("strong")%>%html_text() , 
+                            value=html_nodes(webpage,"li.specs-item")%>%html_nodes("span")%>%html_text())
+  list_res[[i]]<-bind_rows(list_res[[i]],data.frame(specs=c("phone","precio"),
+                                                    value=c(html_nodes(webpage,"span.profile-info-phone-value")%>%str_extract_all("[0-9]+")%>%{map(.,~paste0(.,collapse=""))}%>%unlist%>%paste0(collapse=";"),
+                                                            html_nodes(webpage,"span.price-tag-fraction")%>%str_extract_all("[0-9]+")%>%unlist%>%paste(collapse=""))))
+  list_res[[i]][,1]%>%str_replace_all(" ",".")->list_res[[i]][,1]
+}
+
+list_res%>%map(~.[,1])%>%reduce(union)->atributes_vec
+
+res<-data.frame(matrix(NA,length(z0),length(atributes_vec)+1,dimnames=list(c(), c(atributes_vec,"http"))))
+
+for(i in seq_along(z0)){
+  res[i,list_res[[i]][,1]]<-list_res[[i]][,2]
+  res[i,"http"]<-z0[i]
+}
 
