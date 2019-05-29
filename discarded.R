7b74d35fd6cee8b0c278f605caf6ca19e41c8686
nmolanog <nmolanog@gmail.com>
2019-05-29 10:57:47 -0500

    last sarch :(

--- discarded.R
+++ discarded.R
@@ -9,7 +9,7 @@ library(xml2)
 library(rvest)
 library(stringr)
 
-discarded_placa<-c("RHU-514","COC-844","QIA010","OBI-928","HKY-612","DDO-390","SXY-546","DIU-204","KBO274","SWT 328")
+discarded_placa<-c("RHU-514","COC-844","QIA010","OBI-928","HKY-612","DDO-390","SXY-546","DIU-204","KBO274","SWT 328","IFW-662","CRD 4X4","HSZ 438")
 discarded_phones<-c("16950576","3209514205")
 discard_list<-list(placa=discarded_placa,phones=discarded_phones)
 save(discard_list,file=paste0("discard_list",".RData"))
